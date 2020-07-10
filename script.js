88c90c028828f9b888a05c8de5254e180add7dbc
Matt Stalzer <mjs58181@uga.edu>
2020-07-09 23:10:25 -0400

    local storage/score/timer issues

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// questions that the user will loop through \\
 var questions = [
     {
         question : "What is the capital of Georgia?",
@@ -7,7 +8,7 @@ var questions = [
         choiceD : "Savannah",
         correct : "A"
     },{
-        question : "Where was pizza invented",
+        question : "Where was pizza invented?",
         choiceA : "Rome",
         choiceB : "Vennice",
         choiceC : "Naples",
@@ -58,8 +59,8 @@ var questions = [
     },
 ];
 
-
-var startButton = document.querySelector("button")
+// some global variables from html page \\
+var startButton = document.querySelector("button");
 var quiz = document.getElementById("quiz");
 var score = document.getElementById("score")
 var question = document.getElementById("question");
@@ -67,14 +68,16 @@ var choiceA = document.getElementById("A");
 var choiceB = document.getElementById("B");
 var choiceC = document.getElementById("C");
 var choiceD = document.getElementById("D");
-var submitForm = document.getElementById("submitForm")
-var correct = document.getElementById("correct")
-var timeleft = 30;
+var submitForm = document.getElementById("submitForm");
+var correct = document.getElementById("correct");
+// variable for the time and score \\ 
+var timeleft = 15;
 
+// these two variables are used to allow the transition from one question to the next \\
 var lastQuestion = questions.length - 1;
 var currentQuestion = 0;
 
-// render a question
+// this presents all the elements of the questions to the user \\ 
 function newQuestion(){
     var q = questions[currentQuestion];
     
@@ -83,58 +86,125 @@ function newQuestion(){
     choiceB.innerHTML = q.choiceB;
     choiceC.innerHTML = q.choiceC;
     choiceD.innerHTML = q.choiceD;
-}
+    }
 
+// the event listener here starts the interactive quiz \\
 startButton.addEventListener("click" , startQuiz);
 
-// start quiz
+// starts quiz \\
 function startQuiz(){
     
     start.style.display = "none";
       
     quiz.style.display = "block";
     
-setInterval(function(timer){
-      if(timeleft < 0){
-        clearInterval(timer);
-      }
-      else if (timeleft === 0) {
-            quiz.style.display = "none";
-
-            submitForm.style.display = "block";
-
-      }
-      score.textContent = "Time: " + timeleft;
-      timeleft --;
-    }, 1000);
-
-    newQuestion();
-
+    setInterval(function(timer){
+        if(timeleft < 0){
+            console.log(setInterval)
+          clearInterval(timer);
+        }
+        else if (timeleft === 0) {
+              quiz.style.display = "none";
+    
+              location.replace("form.html")
+              finalScore.innerHTML = "Your final score is: " + timeLeft
+    
+        }
+        score.textContent = "Time: " + timeleft;
+        timeleft --;
+      }, 1000);
+    
     checkAnswer();
 
+    nextQuestion();
 }
 
+// this function compares the user click to the correct answer \\ 
 function checkAnswer(answer) {
     if (answer === questions[currentQuestion].correct) {
-            correct.innerHTML = "correct!"
-            currentQuestion++;
-            newQuestion();
-            setTimeout();
+            // correct.innerHTML = "correct!"
+            nextQuestion();
+            // setTimeout();
     }
-    else if (!answer){
+    if (!answer){
             correct.innerHTML = ""
     } 
-    else {
-        correct.innerHTML = "wrong!"
-        timeleft - 3 ;
-        currentQuestion++;
-        newQuestion();
-        setTimeout();
+    else{
+        // correct.innerHTML = "wrong!"
+        timeleft - 5 ;
+        nextQuestion();
+        // setTimeout();
     }
 }
 
+// this function allows the transition from one question to the next, until completion \\
+function nextQuestion(){
+    if (currentQuestion < lastQuestion){
+        currentQuestion++;
+        newQuestion();
+    }
+    else {
+        location.replace("form.html")
+    }
+}
+
+// this function removes the correct or wrong response after one second \\
+// setTimeout(function(){
+//     correct.remove();
+//   }, 1000);
+
+var finalScore = document.getElementById("finalScore");
+var submitButton = document.getElementById("submitButton");
+var finalScore = document.getElementById("finalScore");
+
+var nameInput = document.getElementById("inlineFormInput");
+var nameForm = document.getElementById("submitForm");
+var nameList = document.getElementById("nameList");
+
+var initials = []
+var highscores = []
 
 
-setTimeout(function(){
-    correct.remove();
-  }, 5000);
+init();
+
+function renderScores() {
+    nameList.innerHTML = ""
+
+    for (var i = 0; i < initials.length; i++) {
+    var name = initials[i]   
+    var li = document.createElement("li");
+    li.textContent = name;
+    li.setAttribute("data-index", i);
+    nameList.appendChild(li);
+
+    }
+}
+
+function init() {
+    var storedInitials = JSON.parse(localStorage.getItem("initials"));
+  
+    if (storedInitials !== null) {
+      initials = storedInitials;
+    }
+
+    renderScores();
+  }
+ 
+function storeInitials() {
+    localStorage.setItem("initals" , JSON.stringify(initials))
+}
+
+submitButton.addEventListener("click", function(event) {
+    event.preventDefault();
+  
+    var initialsText = nameInput.value.trim();
+  
+    if (initialsText === "") {
+      return;
+    }
+      initials.push(initialsText);
+      nameInput.value = "";
+
+      storeInitials();
+      renderScores();
+    });
